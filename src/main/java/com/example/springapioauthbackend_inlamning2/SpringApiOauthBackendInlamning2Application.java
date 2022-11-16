fc00db4a7b7aa348974a95438daf7d45901d4b58
Linda <linda.bertling@utb.ecutbildning.se>
2022-11-16 14:46:38 +0100

    initial commit

---
+++ src/main/java/com/example/springapioauthbackend_inlamning2/SpringApiOauthBackendInlamning2Application.java
@@ -0,0 +1,13 @@
+package com.example.springapioauthbackend_inlamning2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiOauthBackendInlamning2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiOauthBackendInlamning2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springapioauthbackend_inlamning2/SpringApiOauthBackendInlamning2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapioauthbackend_inlamning2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiOauthBackendInlamning2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
